4bb4c4e8bb7ecb530ea734157b3ace8cf2af824d
Santiago Bernal <sabernal93@gmail.com>
2017-11-20 18:31:39 +0100

    Updates DiggerCoordinators RequesterBehaviour to extend the BaseRequesterBehvaiour

--- src/behaviour/digger/coordinator/RequesterBehaviour.java
+++ src/behaviour/digger/coordinator/RequesterBehaviour.java
@@ -5,17 +5,14 @@ import java.util.List;
 import agent.AgentType;
 import agent.DiggerAgent;
 import agent.DiggerCoordinatorAgent;
+import behaviour.BaseRequesterBehaviour;
 import jade.core.AID;
-import jade.domain.FIPANames.InteractionProtocol;
-import jade.wrapper.AgentContainer;
 import jade.lang.acl.ACLMessage;
-import jade.proto.AchieveREInitiator;
+import jade.wrapper.AgentContainer;
 import jade.wrapper.AgentController;
-import jade.wrapper.ControllerException;
 import jade.wrapper.StaleProxyException;
 import map.Cell;
 import onthology.GameSettings;
-import onthology.MessageContent;
 
 /**
  * The requester behaviour for the Digger Coordinator Agent. 
@@ -27,32 +24,20 @@ import onthology.MessageContent;
  * 
  *
  */
-public class RequesterBehaviour extends AchieveREInitiator{
+public class RequesterBehaviour extends BaseRequesterBehaviour<DiggerCoordinatorAgent>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public RequesterBehaviour(DiggerCoordinatorAgent agent, ACLMessage msg) {
 		super(agent, msg);
         agent.log("Started behaviour to deal with AGREEs");
 	}
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	
-
     /**
-     * Handle AGREE messages
-     *
-     * @param msg Message to handle
-     */
-    @Override
-    protected void handleAgree(ACLMessage msg) {
-        DiggerCoordinatorAgent agent = (DiggerCoordinatorAgent) this.getAgent();
-        agent.log("AGREE received from " + ((AID) msg.getSender()).getLocalName());
-    }
-
-    /**
-     * Handle INFORM messages
+     * Overriding Inform messages
      *
      * @param msg Message
      */
@@ -93,51 +78,5 @@ public class RequesterBehaviour extends AchieveREInitiator{
     	return null;
     }
 
-    /**
-     * Handle NOT-UNDERSTOOD messages
-     *
-     * @param msg Message
-     */
-    @Override
-    protected void handleNotUnderstood(ACLMessage msg) {
-    	DiggerCoordinatorAgent agent = (DiggerCoordinatorAgent) this.getAgent();
-        agent.log("This message NOT UNDERSTOOD.");
-    }
-
-    /**
-     * Handle FAILURE messages
-     *
-     * @param msg Message
-     */
-    @Override
-    protected void handleFailure(ACLMessage msg) {
-    	DiggerCoordinatorAgent agent = (DiggerCoordinatorAgent) this.getAgent();
-        agent.log("The action has failed.");
-       /* ACLMessage initialRequest = new ACLMessage(ACLMessage.REQUEST);
-        initialRequest.clearAllReceiver();
-        initialRequest.addReceiver(agent.getAID());
-        initialRequest.setProtocol(InteractionProtocol.FIPA_REQUEST);
-        agent.log("Request message to agent");
-        try {
-            initialRequest.setContent(MessageContent.GET_MAP);
-            agent.log("Request message content:" + initialRequest.getContent());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        reset(initialRequest);
-        agent.addBehaviour(this);*/
-
-    } //End of handleFailure
-
-    /**
-     * Handle REFUSE messages
-     *
-     * @param msg Message
-     */
-    @Override
-    protected void handleRefuse(ACLMessage msg) {
-    	DiggerCoordinatorAgent agent = (DiggerCoordinatorAgent) this.getAgent();
-        agent.log("Action refused.");
-    }
 
 }
